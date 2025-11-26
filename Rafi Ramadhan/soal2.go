f43b3f78f894860c77f36c1382f1a2348c293f17
rafiramadhan <rafirama1020@gmail.com>
2025-11-26 19:07:19 +0700

    Create soal2.go

---
+++ Rafi Ramadhan/soal2.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+func main() {
+	var kendaraan string
+	var jam int
+	var tarif int
+	var total int
+
+	fmt.Print("Masukan jenis kendaraan (motor/mobil/truk) : ")
+	fmt.Scan(&kendaraan)
+	fmt.Print("Masukan Durasi Parkir (dalam jam) : ")
+	fmt.Scan(&jam)
+
+	if jam < 1 {
+		jam = 1
+	}
+
+	switch kendaraan {
+	case "motor":
+		tarif = 2000
+	case "mobil":
+		tarif = 5000
+	case "truk":
+		tarif = 8000
+	default:
+		fmt.Println("Jenis Kendaraan Tidak Valid")
+		return
+	}
+
+	total = tarif * jam
+
+	fmt.Printf("Total Tarif Parkir: Rp %d\n", total)
+}
