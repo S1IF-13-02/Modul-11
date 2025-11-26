df7eee8a3e0d3909446d21fa8b036ce73e6f3cad
rafiramadhan <rafirama1020@gmail.com>
2025-11-26 19:01:04 +0700

    Create guide2.go

---
+++ Rafi Ramadhan/guide2.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var tanaman string
+	fmt.Print("Masukkan nama tanaman: ")
+	fmt.Scan(&tanaman)
+
+
+	switch tanaman {
+	case "nepenthes", "drosera":
+		fmt.Println("Termasuk Tanaman Karnivora")
+		fmt.Println("Asli Indonesia")
+	case "venus", "pinguicula", "sarracenia", "darlingtonia":
+		fmt.Println("Termasuk Tanaman Karnivora")
+		fmt.Println("Bukan Asli Indonesia")
+	default:
+		fmt.Println("Tidak termasuk Tanaman Karnivora")
+	}
+}
