c98244319f938954c7b0af3f747406e47dc4f915
manggala-patra <manggala.kluk13@gmail.com>
2025-12-01 00:20:16 +0700

    Add files via upload

---
+++ Manggala Patra Raditya/Guide3.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+func main() {
+	var jenis string
+	var durasi, tarif int
+
+	fmt.Print("jenis kendaraan (motor/mobil/truk): ")
+	fmt.Scanln(&jenis)
+
+	fmt.Print("durasi parkir (dalam jam): ")
+	fmt.Scanln(&durasi)
+
+	switch jenis {
+	case "motor":
+		if durasi >= 1 && durasi <= 2 {
+			tarif = 7000
+		} else if durasi > 2 {
+			tarif = 9000
+		}
+
+	case "mobil":
+		if durasi >= 1 && durasi <= 2 {
+			tarif = 15000
+		} else if durasi > 2 {
+			tarif = 20000
+		}
+
+	case "truk":
+		if durasi >= 1 && durasi <= 2 {
+			tarif = 25000
+		} else if durasi > 2 {
+			tarif = 30000
+		}
+
+	default:
+		fmt.Println("Jenis kendaraan atau durasi parkir tidak valid")
+		return
+	}
+
+	fmt.Println("Tarif Parkir: Rp", tarif)
+}
